df61c24b819a449bf69241b1abd409a4ba3ff073
marc <marc@vilynx.com>
2019-04-20 00:23:49 +0200

    automate config -- xresources

--- src/replace_xresources.py
+++ src/replace_xresources.py
@@ -12,16 +12,8 @@ def replace_xresources( configuration, json_file):
         if 'Xresources' in json_file:
             xresources = json_file['Xresources']
             prnt.prnt( '-s', 'Found the Xresources info in the JSON file')
-            rl.replace_line(configuration['.Xresources'], '*background:', '*background: '+xresources['background'])
-            rl.replace_line(configuration['.Xresources'], '*foreground:', '*foreground: '+xresources['foreground'])
-            rl.replace_line(configuration['.Xresources'], '*cursorColor:', '*cursorColor: '+xresources['cursorcolor'])
-            for i in range(15):
-                rl.replace_line( configuration['.Xresources'], '*color'+str(i)+':', '*color'+str(i)+': '+xresources['color'+str(i)])
-            rl.replace_line( configuration['.Xresources'], 'rofi.color-window:', 'rofi.color-window: '+xresources['rofi.color-window'])
-            rl.replace_line( configuration['.Xresources'], 'rofi.color-normal:', 'rofi.color-normal: '+xresources['rofi.color-normal'])
-            rl.replace_line( configuration['.Xresources'], 'rofi.color-active:', 'rofi.color-active: '+xresources['rofi.color-active'])
-            rl.replace_line( configuration['.Xresources'], 'rofi.color-urgent:', 'rofi.color-urgent: '+xresources['rofi.color-urgent'])
-
+            for key, value in xresources.items():
+                rl.replace_line(configuration['.Xresources'], key+':', key+': '+value)
         else:
             prnt.prnt( '-f', 'Failed to locate the Xresources info in the JSON file')
     else:
